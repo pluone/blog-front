a32bcb5c1b21c802a11f30d842e9e60baef62cf9
Frank Pei <pluone@outlook.com>
2020-08-12 16:20:31 +0800

    获取币价

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="getPrice">获取币价</el-button>
+
+    <div
+      v-for="currency in info"
+      :key="currency.code"
+    >
+    <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal}}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: null
+    }
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
+  },
+  methods: {
+    getPrice() {
+      this.$axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(reponse => (this.info = reponse.data.bpi))
+      .catch(error => {
+        this.$message.error('接口请求异常')
+      })
+    }
+  }
+}
+</script>
