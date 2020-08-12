0a1b1d38c200547500abda360079bc6cee8873f3
Frank Pei <pluone@outlook.com>
2020-08-12 14:41:05 +0800

    开发日期小工具

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import DevTools from '../views/DevTools.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/tools',
+    name: '开发小工具',
+    component: DevTools
   }
 ]
 
---
+++ src/views/DevTools.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="block">
+    <span>日期天数差计算 </span>
+    <el-date-picker
+      v-model="value1"
+      type="daterange"
+      value-format="yyyy-MM-dd"
+      @change="getDayDiff"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+    >
+    </el-date-picker>
+    相差 {{ dayDiff }} 天
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value1: '',
+      dayDiff: '0',
+    }
+  },
+  methods: {
+    getDayDiff() {
+      if (Array.isArray(this.value1) && this.value1.length == 2) {
+        const startDate = this.value1[0]
+        const endDate = this.value1[1]
+        const url =
+          'http://localhost:8001/api/dayDiff?startDate=' +
+          startDate +
+          '&endDate=' +
+          endDate
+        this.$axios.get(url).then((response) => {
+          this.dayDiff = response.data.data
+        })
+      }
+    },
+  },
+}
+</script>
+<style scoped></style>
